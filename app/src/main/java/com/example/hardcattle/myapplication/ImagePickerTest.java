0ab85c7538716428d6b13ddbece64681f47fc633
guchaochao <guchaochao@huiyunit.com>
2019-12-19 10:20:36 +0800

    Base64对图片进行处理

--- app/src/main/java/com/example/hardcattle/myapplication/ImagePickerTest.java
+++ app/src/main/java/com/example/hardcattle/myapplication/ImagePickerTest.java
@@ -6,11 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.blankj.utilcode.util.ToastUtils;
+import com.bumptech.glide.Glide;
 import com.example.hardcattle.Utils.GlideImageLoader;
 import com.lzy.imagepicker.ImagePicker;
 import com.lzy.imagepicker.bean.ImageItem;
@@ -20,6 +25,9 @@ import com.mylhyl.acp.Acp;
 import com.mylhyl.acp.AcpListener;
 import com.mylhyl.acp.AcpOptions;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,11 +39,14 @@ import static com.lzy.imagepicker.ImagePicker.REQUEST_CODE_PREVIEW;
  */
 
 public class ImagePickerTest extends AppCompatActivity implements View.OnClickListener {
-    private Button btnGetPic, btnTakePic;
-    private ImageView ivShow;
+    private static final String TAG = "ImagePickerTest";
+    private Button btnGetPic, btnTakePic, btnToBase64, btnShowBase64;
+    private ImageView ivShow, ivShowBase64;
+    private TextView tvBase64;
     public static final int REQUEST_CODE_SELECT = 100;
     private ImagePicker imagePicker;
     private int maxNum = 1;
+    private String picBase64, picPath;
 
     private ArrayList<ImageItem> images;
 
@@ -49,7 +60,11 @@ public class ImagePickerTest extends AppCompatActivity implements View.OnClickLi
     private void init() {
         btnGetPic = findViewById(R.id.btn_imagepickertest);
         btnTakePic = findViewById(R.id.btn_imagepickertest_take_pic);
+        btnToBase64 = findViewById(R.id.btn_get_base64);
+        btnShowBase64 = findViewById(R.id.btn_glide_show_base64);
         ivShow = findViewById(R.id.iv_imagepickertest);
+        tvBase64 = findViewById(R.id.tv_base64);
+        ivShowBase64 = findViewById(R.id.iv_show);
         imagePicker = ImagePicker.getInstance();
         imagePicker.setImageLoader(new GlideImageLoader());
         imagePicker.setShowCamera(true);
@@ -58,6 +73,8 @@ public class ImagePickerTest extends AppCompatActivity implements View.OnClickLi
         images = new ArrayList<>();
         btnGetPic.setOnClickListener(this);
         btnTakePic.setOnClickListener(this);
+        btnToBase64.setOnClickListener(this);
+        btnShowBase64.setOnClickListener(this);
         ivShow.setOnClickListener(this);
     }
 
@@ -100,7 +117,40 @@ public class ImagePickerTest extends AppCompatActivity implements View.OnClickLi
             case R.id.btn_imagepickertest_take_pic:
                 takePics();
                 break;
+            case R.id.btn_get_base64:
+                if (images == null || images.size() == 0) {
+                    return;
+                } else {
+                    picPath = images.get(0).path;
+                    picBase64 = img2Base64(picPath);
+
+                    tvBase64.setText(picBase64.substring(0, 100) + "...");
+                }
+                break;
+            case R.id.btn_glide_show_base64:
+                if (TextUtils.isEmpty(picBase64)) {
+                    ToastUtils.showShort("没有Base64图片");
+                } else {
+                    byte[] data = Base64.decode(picBase64, Base64.DEFAULT);
+                    Glide.with(this).load(data).into(ivShowBase64);
+                }
+                break;
         }
+    }
+
+    private String img2Base64(String p) {
+        InputStream in = null;
+        byte[] data = null;
+        try {
+            in = new FileInputStream(p);
+            data = new byte[in.available()];
+            in.read(data);
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return Base64.encodeToString(data, Base64.DEFAULT);
     }
 
     /**
